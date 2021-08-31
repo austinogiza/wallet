0dfc91d150bcac56a191fa2c7be2beda2107e714
austinogiza <austinogiza@gmail.com>
2021-08-31 18:19:31 +0100

    build and api updated

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -1,4 +1,4 @@
-export const host = process.env.NODE_ENV === 'development' ? "http://127.0.0.1:8000": null
+export const host = process.env.NODE_ENV === 'development' ? "http://127.0.0.1:8000": "https://api2.wallet.surgery"
 
 
 
